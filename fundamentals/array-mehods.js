055065ed424e26df97be935dbf7756fcadd60ed7
TadeoAraujoFigueroa <tadeoaraujof@gmail.com>
2026-02-17 20:57:24 -0300

    Array methods

--- fundamentals/array-mehods.js
+++ fundamentals/array-mehods.js
@@ -18,3 +18,25 @@ console.log(userNames); // Salida: ['Alice', 'Bob', 'Charlie', 'David', 'Eve', '
 //Funciona igual que el método Where() en C#, filtrando los objetos del array según una condición, en este caso la edad mayor a 30.
 const usersOver30 = users.filter(user => user.age > 30);
 console.log(usersOver30); // Salida: [{ id: 3, name: 'Charlie', age: 35 }, { id: 4, name: 'David', age: 40 }, { id: 5, name: 'Eve', age: 45 }, { id: 6, name: 'Frank', age: 50 }, { id: 7, name: 'Grace', age: 55 }]
+
+//Ejemplo de uso de find para obtener el primer usuario con el nombre 'David'
+//Funciona igual que el método FirstOrDefault() en C#, buscando el primer objeto del array que cumpla una condición, en este caso el nombre sea 'David'.
+const userDavid = users.find(user => user.name === 'David');
+console.log(userDavid); // Salida: { id: 4, name: 'David', age: 40 }
+
+//Ejemplo de uso de reduce para obtener la suma de las edades de los usuarios
+//Basicamente, aplica la función que le pasamos a cada elemento del array, y va acumulando el resultado en el primer parámetro (sum), que inicia con el valor que le pasamos al final (0).
+const totalAge = users.reduce((sum, user) => sum + user.age, 0);
+//El ultimo parámetro (0) es el valor inicial de sum, que en este caso es 0 porque queremos sumar las edades. Si no le pasamos un valor inicial, sum empezaría con el valor del primer elemento del array, lo cual no es lo que queremos en este caso.
+console.log(totalAge); // Salida: 280
+
+//Ejemplo de uso de some para verificar si hay algún usuario menor de 30 años
+//Funciona igual que el método Any() en C#, verificando si al menos un objeto del array cumple una condición, en este caso la edad menor a 30.
+const hasUsersUnder30 = users.some(user => user.age < 30);
+console.log(hasUsersUnder30); // Salida: true
+
+//Ejemplo de uso de every para verificar si todos los usuarios son mayores de 20 años
+//Funciona igual que el método All() en C#, verificando si todos los objetos del array cumplen una condición, en este caso la edad mayor a 20.
+const allUsersOver20 = users.every(user => user.age > 20);
+console.log(allUsersOver20); // Salida: true
+
